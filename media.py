077892246a6ad29d0c4bd365f16f2e02cbd70263
Max Klopsch <m.p.klopsch@gmail.com>
2017-05-05 12:33:50 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+import webbrowser
+
 class Movie():
   def __init__(self, title, storyline, poster_url, trailer_url):
     self.title = title
     self.storyline = storyline
     self.poster = poster_url
     self.trailer = trailer_url
+
+  def show_trailer(self):
+    webbrowser.open(self.trailer)
