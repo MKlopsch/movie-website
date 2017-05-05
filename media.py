b2773ca6667526a7228da13e1cfc45c3b36b0f71
Max Klopsch <m.p.klopsch@gmail.com>
2017-05-05 12:16:34 +0200

    Create media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+  def __init__(self, title, storyline, poster_url, trailer_url):
+    self.title = title
+    self.storyline = storyline
+    self.poster = poster_url
+    self.trailer = trailer_url
